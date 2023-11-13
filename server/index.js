1f25e3ea2399a93e9614066280714e125fbbce1c
Khai <yukhailoh010@gmail.com>
2023-11-13 17:32:29 +0800

    Initialized frontend UI

--- server/index.js
+++ server/index.js
@@ -4,12 +4,15 @@ import { schema } from "./schema/schema.js";
 import { config } from "dotenv";
 config();
 import { connectDB } from "./config/db.js";
+import cors from "cors";
 import colors from "colors";
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 await connectDB();
+
+app.use(cors());
 
 app.use(
   "/graphql",
